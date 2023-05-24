76fc6b8ab5ffa48b6d7ad06c5c00846bce57b7bf
Matt Dunbar <me@mattdunbar.io>
2023-05-24 11:12:06 -0400

    updated gcb

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -57,7 +57,7 @@ alias clearorig="find . -name '*.orig' -delete"
 alias clearignored="git rm -r --cached . && git add ."
 alias repo="npx openup"
 #alias gcb="git checkout \$(git branch | fzf) && clear || clear"
-alias gcb="\$(git fetch --prune && git checkout \$(node ~/.config/scripts/branch.js \$(git branch -a) | fzf) && git pull) && clear || clear"
+alias gcb="\$(git fetch --prune; git checkout \$(node ~/.config/scripts/branch.js \$(git branch -a) | fzf); git pull); clear"
 alias gdb="git branch -D \$(git branch | fzf)"
 
 # iOS
