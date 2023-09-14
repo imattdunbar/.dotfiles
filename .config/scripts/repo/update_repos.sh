f849aed897656c62d08b6a107ac040921a757e7c
Matt Dunbar <me@mattdunbar.io>
2023-09-13 21:38:55 -0400

    Updates

--- .config/scripts/repo/update_repos.sh
+++ .config/scripts/repo/update_repos.sh
@@ -1,4 +1,4 @@
-if [[ $(HOSTNAME) == "DunbarMBP" ]]; then
+if [[ $HOST == *"DunbarMBP"* ]]; then
     rm -rf $HOME/.config/scripts/repo/personal_repos.txt &&
     find ~/Dev -name .git -type d >> $HOME/.config/scripts/repo/personal_repos.txt
 else
