b95e8b19105c8834c58b0bb2ae8f4c74f89327cd
Matt Dunbar <me@mattdunbar.io>
2024-02-29 20:26:09 -0500

    Updates

--- .config/xcode/backup.sh
+++ .config/xcode/backup.sh
@@ -5,11 +5,11 @@ XC_USER_DATA="/Users/$USER/Library/Developer/Xcode/UserData"
 
 THEME_DIR="$XC_USER_DATA/FontAndColorThemes/"
 mkdir -p $THEME_DIR
-cp $THEME_DIR/Dunbar.xccolortheme ./
+cp $THEME_DIR/Dunbar.xccolortheme ~/.config/xcode/Dunbar.xccolortheme
 
 KB_DIR="$XC_USER_DATA/KeyBindings"
-cp $KB_DIR/Default.idekeybindings ./
+cp $KB_DIR/Default.idekeybindings ~/.config/xcode/Default.idekeybindings
 
-cp "$XC_USER_DATA/IDEPreferencesController.xcuserstate" ./IDEPreferencesController.xcuserstate 
+cp "$XC_USER_DATA/IDEPreferencesController.xcuserstate" ~/.config/xcode/IDEPreferencesController.xcuserstate 
 
 echo 'Xcode settings backed up'
