dd69868c84ea89f728ce44a6654665c74d7faed4
Matt Dunbar <me@mattdunbar.io>
2024-02-29 20:17:40 -0500

    Updates

--- .config/xcode/restore.sh
+++ .config/xcode/restore.sh
@@ -2,15 +2,16 @@
 
 USER="$(whoami)"
 XC_USER_DATA="/Users/$USER/Library/Developer/Xcode/UserData"
+XCODE_BACKUP="/Users/$USER/.config/xcode"
 
 THEME_DIR="$XC_USER_DATA/FontAndColorThemes/"
 mkdir -p $THEME_DIR
-cp ./Dunbar.xccolortheme $THEME_DIR
+cp "$XCODE_BACKUP/Dunbar.xccolortheme" $THEME_DIR
 
 KB_DIR="$XC_USER_DATA/KeyBindings/"
 mkdir -p $KB_DIR
-cp ./Default.idekeybindings $KB_DIR
+cp "$XCODE_BACKUP/Default.idekeybindings" $KB_DIR
 
-cp ./IDEPreferencesController.xcuserstate "$XC_USER_DATA"
+cp "$XCODE_BACKUP/IDEPreferencesController.xcuserstate" "$XC_USER_DATA"
 
 echo 'Xcode settings restored'
