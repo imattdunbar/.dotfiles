70b8fe9e3ceeb4fbb67b06ba11e811ec49599c9b
Matt Dunbar <me@mattdunbar.io>
2024-04-01 10:56:10 -0400

    Updates

--- .config/zsh/local.zsh
+++ .config/zsh/local.zsh
@@ -1,2 +1,4 @@
 # Used for machine specific aliases and configs (personal, work, server, etc.)
 # Changes are ignored but this file stays.
+
+alias setup-local-zsh="git update-index --skip-worktree ~/.config/zsh/local.zsh"
