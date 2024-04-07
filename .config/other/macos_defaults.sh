0af1013154c0d15e23981f98aa1ab3fbc3de064c
Matt Dunbar <me@mattdunbar.io>
2024-04-07 17:43:07 -0400

    Updates

--- .config/other/macos_defaults.sh
+++ .config/other/macos_defaults.sh
@@ -34,9 +34,9 @@ defaults write com.apple.screencapture type -string "png"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
 # Disable press-and-hold for keys in favor of key repeat + fast key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 12
+# defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+# defaults write NSGlobalDomain KeyRepeat -int 1
+# defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
 # Enable full keyboard access for all controls
 # (e.g. enable Tab in modal dialogs)
