992b5d828ba6e19d82462b845c62c034ed27a5d5
Matt Dunbar <me@mattdunbar.io>
2026-06-11 07:12:44 -0400

    Updates

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -216,5 +216,6 @@ alias caddy-fmt="caddy fmt --overwrite --config /opt/homebrew/etc/caddy/Caddyfil
 # Misc
 alias restart-raycast="sh ~/.config/other/scripts/restart_raycast.sh"
 alias npmx="open https://npmx.dev"
+alias audiobook="open https://www.audible.com/library"
 
 alias unfuck-macos='killall Dock Finder SystemUIServer ControlCenter NotificationCenter; killall cfprefsd; /System/Library/CoreServices/Dock.app/Contents/MacOS/Dock >/dev/null 2>&1 & disown'
