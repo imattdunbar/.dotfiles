00a33d4e8a503970457b25947efff22c6dd7f644
Matt Dunbar <me@mattdunbar.io>
2026-07-29 15:51:32 -0400

    Updates

--- .config/zsh/herdr.zsh
+++ .config/zsh/herdr.zsh
@@ -14,8 +14,14 @@ alias h-sr="herdr pane split --current --direction right --focus > /dev/null"
 alias h-sd="herdr pane split --current --direction down --focus > /dev/null"
 
 # Big pane on left | 2 split vertically on right
-h-layout() {
+h-layout-left() {
     local pane_id=$(herdr pane split --current --direction right --no-focus | jq -r '.result.pane.pane_id')
     [[ -n "$pane_id" && "$pane_id" != "null" ]] &&
         herdr pane split "$pane_id" --direction down --focus >/dev/null
+}
+
+# Big pane on right | 2 split vertically on left
+h-layout-right() {
+    herdr pane split --current --direction right --no-focus >/dev/null &&
+    herdr pane split --current --direction down --focus >/dev/null
 }
