8ecd825ec5c4036a7194b1b26fa300c3ee65a2a0
Matt Dunbar <me@mattdunbar.io>
2026-08-26 13:32:18 -0400

    Updates

--- .config/zsh/local.example.zsh
+++ .config/zsh/local.example.zsh
@@ -9,3 +9,9 @@ export JAVA_HOME="$HOME/Library/Java/JavaVirtualMachines/jbr-17.0.8/Contents/Hom
 jira() {
   open "https://SOME_JIRA_URL.com/$1"
 }
+
+# Android
+export JAVA_HOME="$HOME/Library/Java/JavaVirtualMachines/jbr-17.0.14/Contents/Home" # default Java
+export GRADLE_LOCAL_JAVA_HOME="$JAVA_HOME"
+export ANDROID_HOME="$HOME/Library/Android/sdk" # adb
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator:$PATH" # adb
